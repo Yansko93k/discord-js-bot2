3201827510e031f007ac574441069133de902ef2
Yansko <122872487+Yansko93k@users.noreply.github.com>
2025-06-21 21:30:09 +0200

    Update deploy-commands.js
    modification deploys commande

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 module.exports = async () => {
   const commands = [];
-  const commandsPath = path.join(__dirname, 'commands');
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+ const commandsPath = path.join(__dirname, 'src', 'commands');
+ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
