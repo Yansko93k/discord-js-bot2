ef60b5bcbf6c23a0a92e171dbb7ccfe8da3aa33b
Yansko <122872487+Yansko93k@users.noreply.github.com>
2025-06-21 19:43:45 +0200

    Update deploy-commands.js
    ajout deploys commande

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,23 +3,21 @@ const path = require('node:path');
 const { REST, Routes } = require('discord.js');
 require('dotenv').config();
 
-const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+module.exports = async () => {
+  const commands = [];
+  const commandsPath = path.join(__dirname, 'commands');
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  if ('data' in command && 'execute' in command) {
-    commands.push(command.data.toJSON());
-  } else {
-    console.log(`[WARNING] Le fichier ${file} ne semble pas avoir une commande valide.`);
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      commands.push(command.data.toJSON());
+    }
   }
-}
 
-const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+  const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
-(async () => {
   try {
     console.log('Déploiement des commandes globales...');
     await rest.put(
@@ -28,6 +26,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
     );
     console.log('Commandes globales déployées avec succès.');
   } catch (error) {
-    console.error(error);
+    console.error('Erreur lors du déploiement des commandes:', error);
+    throw error;
   }
-})();
+};
