082548a4a497ac8db4d313c287875f8b4a00715a
Yansko <122872487+Yansko93k@users.noreply.github.com>
2025-06-21 21:47:00 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,13 +3,27 @@ const path = require('node:path');
 const { REST, Routes } = require('discord.js');
 require('dotenv').config();
 
+function getAllCommandFiles(dirPath, arrayOfFiles = []) {
+  const files = fs.readdirSync(dirPath);
+
+  for (const file of files) {
+    const fullPath = path.join(dirPath, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      arrayOfFiles = getAllCommandFiles(fullPath, arrayOfFiles);
+    } else if (file.endsWith('.js')) {
+      arrayOfFiles.push(fullPath);
+    }
+  }
+
+  return arrayOfFiles;
+}
+
 module.exports = async () => {
   const commands = [];
- const commandsPath = path.join(__dirname, 'src', 'commands');
- const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  const commandsPath = path.join(__dirname, 'src', 'commands');
+  const commandFiles = getAllCommandFiles(commandsPath);
 
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
+  for (const filePath of commandFiles) {
     const command = require(filePath);
     if ('data' in command && 'execute' in command) {
       commands.push(command.data.toJSON());
